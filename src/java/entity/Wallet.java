ace26777a9ce0cade54c8aff76129bb25a6c4956
LIAU WEISHENG RIZZA <rizza.liau.2016@sis.smu.edu.sg>
2018-10-11 22:35:12 +0800

    -Created new wallet Entity

---
+++ src/java/entity/Wallet.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+/**
+ *
+ * @author Rizza
+ */
+public class Wallet {
+    private String id;
+    private Double refundAmount;
+    private Double rebateAmount;
+    private String debtorCode;    
+    
+    public Wallet(){
+        
+        this.id = id;
+        this.refundAmount = refundAmount;
+        this.rebateAmount = rebateAmount;
+        this.debtorCode = debtorCode;
+        
+    }
+    
+    public String getID(){
+        return id;
+    }
+    
+    public Double getRefundAmount(){
+        return refundAmount;
+    }    
+    
+    public Double getRebateAmount(){
+        return rebateAmount;
+    }
+    
+    public String getDebtorCode(){
+        return debtorCode;
+    }
+    
+}
